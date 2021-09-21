b4bd8e3f564e8255ac4244103e599f9562e1442f
NoorMdKhan123 <noormd6400@gmail.com>
2021-09-21 18:09:34 +0530

    Calculating employee wage

---
+++ EmployeeUC1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace EmployeeUC1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            employee = 1;
+
+            if(employee == 1)
+                Console.WriteLine("Employee is present");
+
+            else
+                Console.WriteLine("Employee is absent");
+
+
+        }
+    }
+}
---
+++ EmployeeWage/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace EmployeeWage
+{
+    class Program
+    {
+        /// <summary>
+        /// Defines the entry point of the application.
+        /// </summary>
+        /// <param name="args">The arguments.</param>  
+          static void Main(string[] args)
+        {
+            ///
+
+            int employee = 1;
+            if (employee == 1)
+            {
+                Console.WriteLine("Employee is Present");
+            }
+            else
+            {
+                Console.WriteLine("Employee is absent");
+            }
+        }
+    }
+}
